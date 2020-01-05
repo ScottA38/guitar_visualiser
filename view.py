e9e81cb1f12c73e1766cc56f52e48215947a895e
ScottA94 <S192660@uos.ac.uk>
2020-01-05 20:55:04 +0000

    Encapsulated functonality in App class for widgets

--- view.py
+++ view.py
@@ -11,8 +11,14 @@ class ScaleName(Observable):
         self.l_widget.bind("<Button-1>", self.notifyObservers)
 
     def get_list_selection(self):
-        index = int(self.l_widget.curselection()[0])
-        return self.l_widget.get(index)
+        try:
+            index = int(self.l_widget.curselection()[0])
+            return self.l_widget.get(index)
+        except IndexError:
+            print()
+            raise IndexError(f"Error occurred trying to get the current listbox selection for wrapper class {self.__class__.__name__}.\nThis is likely because a selection has not been set yet.")
+        except:
+            raise Exception(f"Unknown exception encountered when trying to get the current Listbox selection in wrapper class {self.__class__.__name__} ")
 
     def notifyObservers(self, *args):
         self.setChanged()
@@ -36,95 +42,102 @@ class RelativeKey(Observer):
 
     def draw_list(self, scale_name):
         self.l_widget.delete(0, END)
-        for key in scales[scale_name].keys():
+        for key in App.scales[scale_name].keys():
             self.l_widget.insert(END, key)
 
-def draw_visualisation(fretboard, ScaleObj, RKeyObj, frame):
-    """function to take all current input data and draw the fretboard to screen"""
-    cur_scale = Scale("C", scales[ScaleObj.get_list_selection()][RKeyObj.get_list_selection()])
-    cur_scale.impose(fretboard)
-
-    c=0
-    r=0
-    for string in fretboard:
-        for fret in string:
-            background = "brown"
-            foreground = "black"
-            if c == 0:
-                background = "white"
-            if fret.degree != None:
-                #print(f"view.py says fret.degree of fret {fret} is: {fret.degree}") DEBUG
-                if fret.degree == 0:
-                    foreground = "blue"
-                else:
-                    foreground = "green"
-            Label(frame, text=fret, bg = background, fg=foreground, relief=RIDGE, width=5, height=2  ).grid(row=r, column=c)
-            c += 1
-        r += 1
-        c=0
-
-scales = {
-            "pentatonic":
-            {
-                "minor": [3, 2, 2, 3],
-                "major": [2, 2, 3, 2]
-            },
-            "natural":
-            {
-                "minor": [2, 1, 2, 2, 1, 2],
-                "major": [2, 2, 1, 2, 2, 2]
-            },
-            "harmonic":
-            {
-                "minor": [2, 1, 2, 2, 1, 3],
-                "major": [2, 2, 1, 2, 1, 3]
-            },
-            "melodic":
-            {
-                "minor ascending": [2, 1, 2, 2, 2, 2],
-                "minor descending": [2, 2, 1, 2, 2, 1]
+class App(Tk):
+    scales = {
+                "pentatonic":
+                {
+                    "minor": [3, 2, 2, 3],
+                    "major": [2, 2, 3, 2]
+                },
+                "natural":
+                {
+                    "minor": [2, 1, 2, 2, 1, 2],
+                    "major": [2, 2, 1, 2, 2, 2]
+                },
+                "harmonic":
+                {
+                    "minor": [2, 1, 2, 2, 1, 3],
+                    "major": [2, 2, 1, 2, 1, 3]
+                },
+                "melodic":
+                {
+                    "minor ascending": [2, 1, 2, 2, 2, 2],
+                    "minor descending": [2, 2, 1, 2, 2, 1]
+                }
             }
-        }
 
-def redraw(event):
-    """Method for event handling on click inside of header frame - MUST be called exclusively by the 'header' Frame"""
-    print("event fired!")
-    if event.widget.__class__.__name__ == "Frame":
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+        self.title("Fretboard_visualiser")
+
+        #declare widgets to live inside of root
+        self.header = Frame(self, height=100,  bd=2, relief=SUNKEN)
+        self.body = Frame(self, bd=2, relief=SUNKEN)
+        self.scale_names = Listbox(self.header, height=4, exportselection=0, name="scale_names")
+        self.rkey_list = Listbox(self.header, height=4, exportselection=0, name="relative_keys")
+
+        #populate Listboxes
+        for scale in App.scales.keys():
+            self.scale_names.insert(END, scale)
+        for key in App.scales["pentatonic"].keys():
+            self.rkey_list.insert(END, key)
+
+        #bind events
+        self.scale_names.bind("<Button-1>", )
+        self.rkey_list.bind("<Button-1>", self.redraw_body)
+
+        #Listbox Observable/Observer objects
+        self.Obs_scale = ScaleName(self.scale_names)
+        self.Obr_rkey = RelativeKey(self.rkey_list)
+        #attach observer
+        self.Obs_scale.addObserver(self.Obr_rkey)
+
+        #pack everything in order
+        self.header.pack(expand = True, fill="both")
+        self.body.pack(expand = True, fill="both")
+        self.scale_names.pack()
+        self.rkey_list.pack()
+
+        #create fretboard objects
+        self.fretboard = Fretboard(['E', 'A', 'D', 'G', 'B', 'E'], 23)
+        self.scale = None #initialised when selection is made
+        self.current_selection = App.scales[self.Obs_scale.get_list_selection()][self.Obr_rkey.get_list_selection()]
+        self._draw_visualisation()
+
+
+    def _draw_visualisation(self):
+        """function to take all current input data and draw the fretboard to screen"""
+        self.scale = Scale("C", App.scales[self.Obs_scale.get_list_selection()][self.Obr_rkey.get_list_selection()])
+        self.scale.impose(self.fretboard)
+
+        c=0
+        r=0
+        for string in self.fretboard:
+            for fret in string:
+                background = "brown"
+                foreground = "black"
+                if c == 0:
+                    background = "white"
+                if fret.degree != None:
+                    #print(f"view.py says fret.degree of fret {fret} is: {fret.degree}") DEBUG
+                    if fret.degree == 0:
+                        foreground = "blue"
+                    else:
+                        foreground = "green"
+                Label(self.body, text=fret, bg = background, fg=foreground, relief=RIDGE, width=5, height=2  ).grid(row=r, column=c)
+                c += 1
+            r += 1
+            c=0
+
+    def redraw_body(event):
+        """Reconstruct body when click event is fired """
         body.destroy()
         body = Frame(root, bd=2, relief=SUNKEN)
         body.pack(expand=True, fill="both")
-        draw_visualisation(e_std, Obs_ScaleList, Obr_RKeyList, body)
+        self._draw_visualisation(e_std, Obs_ScaleList, Obr_RKeyList, body)
 
-root = Tk()
-root.title("Fretboard_visualiser")
-
-header = Frame(root, height=100,  bd=2, relief=SUNKEN)
-header.pack(expand=True, fill="both")
-
-body = Frame(root, bd=2, relief=SUNKEN)
-body.pack(expand=True, fill="both")
-
-scale_names = Listbox(header, height=4, exportselection=0, name="scale_names")
-relative_key = Listbox(header, height=4, exportselection=0, name="relative_keys")
-
-scale_names.pack()
-relative_key.pack()
-
-header.bind("<Button-1>", redraw)
-
-for scale in scales.keys():
-    scale_names.insert(END, scale)
-
-for key in scales["pentatonic"].keys():
-    relative_key.insert(END, key)
-
-Obs_ScaleList = ScaleName(scale_names)
-Obr_RKeyList = RelativeKey(relative_key)
-
-Obs_ScaleList.addObserver(Obr_RKeyList)
-
-e_std = Fretboard(['E', 'A', 'D', 'G', 'B', 'E'], 23)
-
-draw_visualisation(e_std, Obs_ScaleList, Obr_RKeyList, body)
-
-root.mainloop()
+app = App()
+app.mainloop()
