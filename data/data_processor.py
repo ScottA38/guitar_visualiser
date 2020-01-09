8975b074d69b105326cedf1599de519b4c158b98
ScottA94 <S192660@uos.ac.uk>
2020-01-09 21:55:40 +0000

    Used json_schema_validator library to create schemas for json data stored for app. This is important for a class to interface the data in bringing it into the app

--- data/data_processor.py
+++ data/data_processor.py
@@ -1,21 +1,35 @@
-import json
+from json_schema_validator.shortcuts import *
+import os
+import sys
 
+os.chdir("data")
 
-class DataProcessor:
+in_tests = (("scale_schema.json", "scales.json"), ("tunings_schema.json", "tunings.json"))
 
-    primaries = ["scales", "tunings"]
-
-    def __init__(self, in_file)
-        with open(in_file, 'r') as data_file:
-            self.data = json.load(data_file)
-
-        for key in self.data.keys():
-            assert key in DataProcessor.primaries
-
-        if "tunings" in self.data.keys():
-            if "open" in self.data["tunings"].keys():
-                #ensuring consistent data structure for tunings
-                assert "roots" in self.data["tunings"].keys()
-                for tuning in self.data["tunings"].values():
-                    assert isinstance(tuning, str)
-                    if
+for test in in_tests:
+    print(f"Validating {test}", end="\n\n\n")
+    try:
+        schema = open(test[0], 'r').read()
+        json = open(test[1], 'r').read()
+        validate(schema, json)
+        print(f"'{test[0]}' successfully validated json data: '{test[1]}'")
+    except BaseException as e:
+        raise e
+# class DataProcessor:
+#
+#     primaries = ["scales", "tunings"]
+#
+#     def __init__(self, in_file)
+#         with open(in_file, 'r') as data_file:
+#             self.data = json.load(data_file)
+#
+#         for key in self.data.keys():
+#             assert key in DataProcessor.primaries
+#
+#         if "tunings" in self.data.keys():
+#             if "open" in self.data["tunings"].keys():
+#                 #ensuring consistent data structure for tunings
+#                 assert "roots" in self.data["tunings"].keys()
+#                 for tuning in self.data["tunings"].values():
+#                     assert isinstance(tuning, str)
+#                     if
