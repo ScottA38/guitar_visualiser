b48bb16c8d815d7db4d52008377e10aa72de432d
ScottA94 <94andersonsc@googlemail.com>
2020-01-10 21:03:38 +0000

    Completed json storage system of app source data. Implemented as a library

--- data/data_processor.py
+++ data/data_processor.py
@@ -1,35 +1,24 @@
-from json_schema_validator.shortcuts import *
+from json_schema_validator.schema import Schema
+from json_schema_validator.validator import Validator
 import os
 import sys
+import json
 
-os.chdir("data")
+class DataProcessor:
 
-in_tests = (("scale_schema.json", "scales.json"), ("tunings_schema.json", "tunings.json"))
+    def __init__(self):
+        self._validator = Validator()
+        self.objs = {}
 
-for test in in_tests:
-    print(f"Validating {test}", end="\n\n\n")
-    try:
-        schema = open(test[0], 'r').read()
-        json = open(test[1], 'r').read()
-        validate(schema, json)
-        print(f"'{test[0]}' successfully validated json data: '{test[1]}'")
-    except BaseException as e:
-        raise e
-# class DataProcessor:
-#
-#     primaries = ["scales", "tunings"]
-#
-#     def __init__(self, in_file)
-#         with open(in_file, 'r') as data_file:
-#             self.data = json.load(data_file)
-#
-#         for key in self.data.keys():
-#             assert key in DataProcessor.primaries
-#
-#         if "tunings" in self.data.keys():
-#             if "open" in self.data["tunings"].keys():
-#                 #ensuring consistent data structure for tunings
-#                 assert "roots" in self.data["tunings"].keys()
-#                 for tuning in self.data["tunings"].values():
-#                     assert isinstance(tuning, str)
-#                     if
+    def add_pair(self, dirname):
+        try:
+            json_path = os.path.realpath(os.path.join(dirname, dirname + "s.json" ))
+            schema_path = os.path.realpath(os.path.join(dirname, dirname + "_schema.json" ))
+            with open(schema_path, 'r') as fp_schema:
+                with open(json_path, 'r') as fp_json:
+                    schema_obj = Schema(json.load(fp_schema))
+                    obj = json.load(fp_json)
+                    assert Validator.validate(schema_obj, obj), "JSON document failed schema validation"
+                    self.objs.update({dirname: obj})
+        except BaseException as e:
+            raise e
